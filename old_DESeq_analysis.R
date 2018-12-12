5de04872da6fd329176c97be89ef47a5e4744142
larouchej <45776062+larouchej@users.noreply.github.com>
2018-12-12 12:38:28 -0500

    Update old_DESeq_analysis.R

--- old_DESeq_analysis.R
+++ old_DESeq_analysis.R
@@ -5,12 +5,9 @@ library("DESeq")
 library(plyr)
 
 ##################### import counts table ############################
-##
-## I imported my unfiltered count table after using tximport. Your table can come from HTSeq-count or Rsubread. I think DESeq has built in functions for HTSeq-count data.
-##
 
-#txi.kallisto.counts = read.table(file='txi.kallisto.counts.txt', sep="\t", header=TRUE, row.names = 1)
-count_217_219 <- read.table(file = "217_219_count_table.txt",header = TRUE, sep = '\t') 
+# I generated separate count tables for 221 and 222, and a joined count table for 217 and 219 using HTSeq
+counts <- read.table(file = "217_219_count_table.txt",header = TRUE, sep = '\t') 
 names(count_217_219) <- c("ENSEMBL_ID", "count_217", "count_219")
 count_217_219 <- count_217_219[ ,-c(4) ]
 count_221 <- read.table(file = "221_count_table.txt",header = FALSE, sep = '\t') 
@@ -18,28 +15,25 @@ names(count_221) <- c("ENSEMBL_ID", "count_221")
 count_222 <- read.table(file = "222_count_table.txt",header = FALSE, sep = '\t')
 names(count_222) <- c("ENSEMBL_ID", "count_222")
 
-## import file with ENSEMBL IDs annotated with gene names
-
-withIDs <- read.table(file = 'hisat_geneSymbolsIDs.csv', header = TRUE, sep = ',')
-names(withIDs) <- c("ENSEMBL_ID", "gene_name")
-
+#join all the count tables based on the ENSEMBL_ID
 counts_joined <- join(count_217_219, count_221, by = 'ENSEMBL_ID', type = "inner")
 counts_joined <- join(counts_joined, count_222, by = 'ENSEMBL_ID', type = "inner")
 #counts_joined <- join(counts_joined, withIDs, by = 'ENSEMBL_ID', type = "inner")
 
+#generate matrix for DESeq analysis
 hisat.countData <- sapply(counts_joined, as.integer, USE.NAMES = TRUE)
 rownames(hisat.countData) <- counts_joined$ENSEMBL_ID
-colnames(counts_joined)<- c("ENSEMBL_ID", "AgedSyn","AdultSyn","AdultCntrl", "AgedCntrl")
 colnames(hisat.countData)<- c("ENSEMBL_ID", "AgedSyn","AdultSyn","AdultCntrl", "AgedCntrl")
 hisat.countData <- hisat.countData[,-1]
 hisat.countData <- hisat.countData[,-5]
 hisat.countData <- hisat.countData[,-5]
-head(hisat.countData)
+# remove NA values
 hisat.countData[is.na(hisat.countData)] <- 0 
 
-## in the end, my count data table looks like this.
+head(hisat.countData)
+## in the end, count data table looks like this.
 
-#                       X217_Aged_Syn X219_Adult_Syn X221_Adult_Ctrl X222_Aged_Ctrl
+#                             AgedSyn       AdultSyn       AdultCtrl       AgedCtrl
 # ENSMUSG00000000001.4           1757           1107            2342           2006
 # ENSMUSG00000000003.13             0              0               0              0
 # ENSMUSG00000000028.12            77             80             122             47
@@ -47,9 +41,9 @@ hisat.countData[is.na(hisat.countData)] <- 0
 # ENSMUSG00000000037.14            41             35              61             30
 # ENSMUSG00000000049.9            108             11              12              5
 
-################## make the samples table as before ######################
+################## make the samples table ######################
 ##
-## add another column with the contrast we did before (AgedSyn vs AdultSyn)
+## add another column with the contrast (AgedSyn vs AdultSyn)
 ##
 
 samples = data.frame(row.names = c("AgedSyn","AdultSyn","AdultCntrl", "AgedCntrl"),
@@ -113,8 +107,8 @@ length(unique(geneid))==length(rownames(counts_joined)) # making sure that we're
 rownames(counts_joined) = geneid
 head(rownames(counts_joined))
 
-#rownames(counts_joined) <- counts_joined[,1]
 
+#### Add another columns with the gene names and ENTREZ IDs #####
 columns(org.Mm.eg.db)
 counts_joined$symbol = mapIds(org.Mm.eg.db,
                         keys=rownames(counts_joined),
@@ -126,10 +120,11 @@ counts_joined$entrez <- mapIds(org.Mm.eg.db,
                          column="ENTREZID",
                          keytype="ENSEMBL",
                          multiVals="first")
-#resOrdered <- counts_joined[order(counts_joined$pvalue),]
-#top100genes <- rownames(resOrdered[1:100,])
+resOrdered <- counts_joined[order(counts_joined$pvalue),]
+top100genes <- rownames(resOrdered[1:100,])
 
-############# Immitating their heatmap
+############# Immitate heatmap from publication ###########
+#Find the locations of each gene of interest
 ache = match('Ache', counts_joined$symbol)
 musk = match('Musk', counts_joined$symbol)
 hdac4 = match('Hdac4', counts_joined$symbol)
@@ -150,29 +145,12 @@ fst = match('Fst', counts_joined$symbol)
 
 keep20 <- c(ache,musk,hdac4,lrp4,chrna1,myog,myod,ppargc1a,esrra,mfn2,opa1,pgp,itgb1,gadd45a,mrpl15,myl4,fst)
 keep20
-cds <- cds[keep20,]
+cds <- cds[keep20,] #index the DESeq results to select genes of interest
 
-cds = estimateSizeFactors( cds )
-sizeFactors(cds)
-head( counts( cds, normalized=TRUE ) )
-
-cds = estimateDispersions(  cds, method="blind", sharingMode="fit-only" )
-str( fitInfo(cds) )
-plotDispEsts( cds )
-
-res = nbinomTest( cds, "AgedSyn", "AdultSyn" )
-head(res)
-head(res[order(res$padj),])
-addmargins( table( res_sig = res$padj < .05) )
-
+#generate heatmap
 vsd = varianceStabilizingTransformation( cds )
 library("RColorBrewer")
 library("gplots")
-hmcol = colorRampPalette(brewer.pal(9, "GnBu"))(100)
-heatmap.2(exprs(vsd), col = hmcol, trace="none", margins = c(8,13))
-
-
-##### Pathway Analysis
-#if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-#BiocManager::install("gage", version = "3.8")
+hmcol = colorRampPalette(brewer.pal(9, "GnBu"))(100) #select a color palet friendly to red-green color blindness
+heatmap.2(exprs(vsd), col = hmcol, trace="none", margins = c(8,13), Colv = FALSE, Rowv = FALSE) 
+#Setting Colv and Rowv to NULL or FALSE keeps the original order of the genes by preventing the function from generating dendrograms
